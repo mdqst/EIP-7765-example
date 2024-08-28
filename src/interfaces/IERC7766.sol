19daff48a2fb1ab172f648f0d6fbe4b352102e7d
frank <frank@frankdeMacBook-Pro.local>
2024-08-28 17:03:11 +0800

    init

---
+++ src/interfaces/IERC7766.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+/// @title ERC-7766 Privileged Non-Fungible Tokens Tied To Real World Assets
+/// @dev See https://eips.ethereum.org/EIPS/eip-7766
+interface IERC7766 /* is IERC721, IERC165 */ {
+
+    /// @notice This event emitted when a specific privilege of a token is successfully exercised.
+    /// @param _operator  the address who exercised the privilege.
+    /// @param _to  the address to benifit from the privilege.
+    /// @param _tokenId  the NFT tokenID.
+    /// @param _privilegeId  the ID of the privileges.
+    event PrivilegeExercised(
+        address indexed _operator,
+        address indexed _to,
+        uint256 indexed _tokenId,
+        uint256 _privilegeId
+    );
+
+    /// @notice This function exercised a specific privilege of a token if succeeds.
+    /// @dev Throws if `_privilegeId` is not a valid privilegeId.
+    /// @param _to  the address to benifit from the privilege.
+    /// @param _tokenId  the NFT tokenID.
+    /// @param _privilegeId  the ID of the privileges.
+    /// @param _data  extra data passed in for extra message or future extension.
+    function exercisePrivilege(
+        address _to,
+        uint256 _tokenId,
+        uint256 _privilegeId,
+        bytes calldata _data
+    ) external returns (bool _success);
+
+    /// @notice This function is to check whether a specific privilege of a token can be exercised.
+    /// @dev Throws if `_privilegeId` is not a valid privilegeId.
+    /// @param _to  the address to benifit from the privilege.
+    /// @param _tokenId  the NFT tokenID.
+    /// @param _privilegeId  the ID of the privileges.
+    function isExercisable(
+        address _to,
+        uint256 _tokenId,
+        uint256 _privilegeId
+    ) external view returns (bool _exercisable);
+
+    /// @notice This function is to check whether a specific privilege of a token has been exercised.
+    /// @dev Throws if `_privilegeId` is not a valid privilegeId.
+    /// @param _to  the address to benifit from the privilege.
+    /// @param _tokenId  the NFT tokenID.
+    /// @param _privilegeId  the ID of the privileges.
+    function isExercised(
+        address _to,
+        uint256 _tokenId,
+        uint256 _privilegeId
+    ) external view returns (bool _exercised);
+
+    /// @notice This function is to list all privilegeIds of a token.
+    /// @param _tokenId  the NFT tokenID.
+    function getPrivilegeIds(
+        uint256 _tokenId
+    ) external view returns (uint256[] memory privilegeIds);
+
+}
---
+++ src/interfaces/IERC7766Metadata.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+/// @title ERC-7766 Privileged Non-Fungible Tokens Tied To Real World Assets, optional metadata extension
+/// @dev See https://eips.ethereum.org/EIPS/eip-7766
+interface IERC7766Metadata /* is IERC7766 */ {
+
+    /// @notice A distinct Uniform Resource Identifier (URI) for a given privilegeId.
+    /// @dev Throws if `_privilegeId` is not a valid privilegeId. URIs are defined in RFC
+    ///  3986. The URI may point to a JSON file that conforms to the "ERC-7766
+    ///  Metadata JSON Schema".
+    function privilegeURI(uint256 _privilegeId) external view returns (string memory);
+
+}
